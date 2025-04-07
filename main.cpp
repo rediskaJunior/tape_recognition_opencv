db241089b731952c472e4213240bcc9f020b2f1c
Alina Babenko <117819807+cyberprince2201@users.noreply.github.com>
2025-04-07 12:54:20 +0300

    First try with OpenCV. Installing and trying demo version.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Alina on 07.04.2025.
+//
+
+#include <opencv2/opencv.hpp>
+
+int main(){
+//    cv::VideoCapture cap(0);
+//    cv::Mat frame;
+//    while (cap >> frame){
+//        cv::imshow("Frame", frame);
+//        cv::waitKey(30);
+//    }
+//    cap.release();
+//    return 0;
+
+    return 0;
+}
---
+++ src/recognition.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alina on 07.04.2025.
+//
+
+#include "../includes/recognition.hpp"
